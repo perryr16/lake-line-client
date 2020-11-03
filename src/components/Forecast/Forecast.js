e039e6798f0cc1ee736ee46d7f9748359f1fa9d6
Ross Perry <perryr16@gmail.com>
2020-11-03 14:04:57 -0700

    dotenv variables must have REACT_APP_ infront

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Conditions from '../Conditions/Conditions';
-const { RAPID_KEY } = process.env
 
 
 const Forecast = () => {
@@ -10,23 +9,21 @@ const Forecast = () => {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": process.env.RAPID_KEY
-        // "x-rapidapi-key": "cc670b05f4msh6a6e141718aa225p1d2171jsn22e5689cf60b"
+        "x-rapidapi-key": process.env.REACT_APP_RAPID_KEY
       }
     })
       .then(response => response.json())
       .then(response => {
         setResponseObj(response)
-        console.log(RAPID_KEY)
-        console.log(process.env.RAPID_KEY)
-        console.log(process.env.MAP_KEY)
+        console.log(process.env.REACT_APP_RAPID_KEY)
+        console.log(process.env.REACT_APP_MAP_KEY)
       })
       .catch(err => {
         console.log(err);
       });
   }
   return (
-    <div class="">
+    <div>
       <h2>Find Current Weather Conditions</h2>
       <button onClick={getForecast}>Get Forecast</button>
       <Conditions 
