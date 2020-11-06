07e911c3b3590b6f57df2a4d405f605f78fa6422
Ross Perry <perryr16@gmail.com>
2020-11-06 11:40:55 -0700

    formatting for trail page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,6 @@ const Navigation = () => {
       <NavLink to='/forecast'>Forecast</NavLink>
       <NavLink to='/trails'>Trails</NavLink> 
       <NavLink to='/addTrails'>Add Trails</NavLink>
-      <NavLink to='/trails/:id'>Add Trails</NavLink>
     </div>
   );
 }
--- src/components/Trails/Trail.js
+++ src/components/Trails/Trail.js
@@ -11,18 +11,19 @@ const trail = (props) => {
   return (
     <div>
       {props.trailResponse.length > 1 ?
-        <div className="trailList">
+        <div className="grid-2">
           {props.trailResponse.forEach(trail => latLons+=`${trail.lat},${trail.lon}||`)}
           <img src={`https://www.mapquestapi.com/staticmap/v5/map?locations=${latLons}&key=${process.env.REACT_APP_MAP_KEY}`} alt=""></img>
-
-          {props.trailResponse.map(trail => 
-          <p>
-          <NavLink className="trailLink" to={`/trails/${trail.id}`}>{trail.name}</NavLink> <br/>
-          <a className="trailLink" href={trail.url}>{trail.url}</a> <br></br>
-          {trail.length} miles  <br></br>
-          {trail.lat}, {trail.lon}
-          </p>
-          )}
+          <div className="scrollingBox">
+            {props.trailResponse.map(trail => 
+            <p>
+            <NavLink className="trailLink" to={`/trails/${trail.id}`}>{trail.name}</NavLink> <br/>
+            <a className="trailLink" href={trail.url}>{trail.url}</a> <br></br>
+            {trail.length} miles  <br></br>
+            {trail.lat}, {trail.lon}
+            </p>
+            )}
+          </div>
         </div>
         : null
       }
